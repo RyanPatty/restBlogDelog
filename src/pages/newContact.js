d8fb353ac44db0f561c231757ade0fe8eef93259
Ryan <41839897+RyanPatty@users.noreply.github.com>
2022-02-11 18:40:53 -0500

    Update newContact.js

--- src/pages/newContact.js
+++ src/pages/newContact.js
@@ -1,1 +1,43 @@
+import React from "react"
+import Helmet from "react-helmet"
+import { graphql } from 'gatsby'
+import Layout from "../components/layout"
+
+const ContactPage = ({
+  data: {
+    site
+  },
+}) => {
+  return (
+    <Layout>
+      <Helmet>
+        <title>newContact — {site.siteMetadata.title}</title>
+        <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
+      </Helmet>
+      <div className="two-grids -contact">
+        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
+          <h1 className="post-title">Get in Touch</h1>
+          <p>Let me help you kick start your next project &rarr;</p>
+        </div>
+
+        <div>
+          <p>Ello Gubnah!</p>
+        </div>
+
+
+      </div>
+    </Layout>
+  )
+}
+export default ContactPage
+export const pageQuery = graphql`
+  query ContactPageQuery{
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
 
