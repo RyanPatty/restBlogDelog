b7e970824fecf6014394559983e6613939bb165c
Ryan <41839897+RyanPatty@users.noreply.github.com>
2022-02-11 20:16:01 -0500

    Update heroHeader.js

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -21,8 +21,8 @@ export default () => (
           className="primary-content" 
           dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
         />
-       // <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
-      //  <Link to='/newcontact' className="button -primary">Get in touch &rarr;</Link>
+       <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
+       <Link to ="https://www.ubereats.com/store/shangrila-bistro/9qOxJCKIRFGR-o53jEbXLA?diningMode=DELIVERY" className="button -primary">Uber Eats Delivery &rarr;</Link>
       </div>
     )}
   />
